7c5e498c6b6e3f2309da7ff331de9ee0c227f503
mohfaz <mobayen.mohammad@gmail.com>
2021-02-12 00:31:03 -0500

    new plot for distilation and filtering is added

--- python_plot.m
+++ python_plot.m
@@ -74,3 +74,26 @@ plot(waiting(1:7),analytical_fidelity(1:7),'-*', 'LineWidth',4)
 % plot(waiting(1:7),analytical_fidelity(1:7)' - fidelity_list(1:7),'-*')
 plot(waiting(1:7),exp(-waiting(1:7)/1e-3))
 legend({'simulated asyng error','analytical async error', 'analytical sync error'},'FontSize',14)
+%% plot for the puyrify.py for different distances for only filtering
+%  number of experiments is 1e+5
+fidelity_list = [0.919945, 0.909385, 0.88037, 0.75431, 0.68326, 0.62268, 0.573045];
+    distances = [1,2,5,20,30,40,50]; % in km
+    plot(distances,fidelity_list,'-O','LineWidth',4)
+    title('Purifying with filtring')
+    grid on
+%% plot for the puyrify.py for different distance adding several memories
+% for filtering and distilation
+%  number of experiments is 1e+5
+fidelity_list_distilation =[0.988, 0.978, 0.95, 0.811, 0.726, 0.659, 0.608];
+fidelity_list_filtering = [0.991, 0.98, 0.942, 0.801, 0.726, 0.632, 0.595];
+distances = [1,2,5,20,30,40,50]; % in km
+plot(distances,fidelity_list_distilation,'-O','LineWidth',4)
+hold on
+plot(distances,fidelity_list_filtering,'--*','LineWidth',4)
+title('Purifying with filtring and distilation')
+grid on
+xlabel('Time ','FontSize',14);
+ylabel('f_0','FontSize',14);
+legend({'Distilation','Local Filtering'},'FontSize',14)
+
+    
