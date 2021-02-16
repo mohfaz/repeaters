f855339bb97dd3268155e7cce4d6e6a0aa8cea45
mohfaz <mobayen.mohammad@gmail.com>
2021-02-16 13:45:05 -0500

    New plot for distilation vs filtering vs just entanglement is added

--- python_plot.m
+++ python_plot.m
@@ -86,14 +86,23 @@ fidelity_list = [0.919945, 0.909385, 0.88037, 0.75431, 0.68326, 0.62268, 0.57304
 %  number of experiments is 1e+5
 fidelity_list_distilation =[0.988, 0.978, 0.95, 0.811, 0.726, 0.659, 0.608];
 fidelity_list_filtering = [0.991, 0.98, 0.942, 0.801, 0.726, 0.632, 0.595];
+fidelity_list = [0.9949999999999999,
+ 0.9839999999999999,
+ 0.9689999999999999,
+ 0.8799999999999999,
+ 0.8289999999999998,
+ 0.7499999999999999,
+ 0.6999999999999998];
 distances = [1,2,5,20,30,40,50]; % in km
 plot(distances,fidelity_list_distilation,'-O','LineWidth',4)
 hold on
 plot(distances,fidelity_list_filtering,'--*','LineWidth',4)
+
 title('Purifying with filtring and distilation')
 grid on
-xlabel('Time ','FontSize',14);
+xlabel('Distance in km ','FontSize',14);
 ylabel('f_0','FontSize',14);
-legend({'Distilation','Local Filtering'},'FontSize',14)
+plot(distances,fidelity_list,'--','LineWidth',4)
+legend({'Distilation','Local Filtering','Just entangling'},'FontSize',14)
 
     
